40723627bc687bcfc7643ab4ac694ba90cb4d30a
dilipkumar <dilipbalaut@gmail.com>
2020-09-08 15:24:33 +0530

    Built-in compression method
    Add syntax allowing a compression method to be specified.
    As of now there is only 2 option for build-in compression
    method (pglz, lz4) which can be set while creating a table
    or adding a new column.  No option for altering the
    compression method for an existing column.
    
    Dilip Kumar based on the patches from Ildus Kurbangaliev.
    Design input from Robert Haas and Tomas Vondra.
    Reviewed by Robert Haas, Tomas Vondra, Alexander Korotkov and Justin Pryzby
    
    Discussions:
    https://www.postgresql.org/message-id/20171213151818.75a20259@postgrespro.ru
    https://www.postgresql.org/message-id/CA%2BTgmoaKDW1Oi9V%3Djc9hOGyf77NbkNEABuqgHD1Cq%3D%3D1QsOcxg%40mail.gmail.com
    https://www.postgresql.org/message-id/CA%2BTgmobSDVgUage9qQ5P_%3DF_9jaMkCgyKxUQGtFQU7oN4kX-AA%40mail.gmail.com
    https://www.postgresql.org/message-id/20201005160355.byp74sh3ejsv7wrj%40development
    https://www.postgresql.org/message-id/CAFiTN-tzTTT2oqWdRGLv1dvvS5MC1W%2BLE%2B3bqWPJUZj4GnHOJg%40mail.gmail.com

--- src/include/nodes/nodes.h
+++ src/include/nodes/nodes.h
@@ -510,6 +510,7 @@ typedef enum NodeTag
 	T_IndexAmRoutine,			/* in access/amapi.h */
 	T_TableAmRoutine,			/* in access/tableam.h */
 	T_TsmRoutine,				/* in access/tsmapi.h */
+	T_CompressionAmRoutine,		/* in access/compressamapi.h */
 	T_ForeignKeyCacheInfo,		/* in utils/rel.h */
 	T_CallContext,				/* in nodes/parsenodes.h */
 	T_SupportRequestSimplify,	/* in nodes/supportnodes.h */

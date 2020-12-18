ce2377650be4ab04bb48183e7f2fdc86ef8c599b
dilipkumar <dilipbalaut@gmail.com>
2020-12-18 11:31:22 +0530

    alter table set compression
    Add support for changing the compression method associated
    with a column, forcing a table rewrite.
    
    Dilip Kumar based on the patches from Ildus Kurbangaliev.
    Design input from Robert Haas and Tomas Vondra.
    Reviewed by Robert Haas, Tomas Vondra, Alexander Korotkov
    
    Discussions:
    https://www.postgresql.org/message-id/20171213151818.75a20259@postgrespro.ru
    https://www.postgresql.org/message-id/CA%2BTgmoaKDW1Oi9V%3Djc9hOGyf77NbkNEABuqgHD1Cq%3D%3D1QsOcxg%40mail.gmail.com
    https://www.postgresql.org/message-id/CA%2BTgmobSDVgUage9qQ5P_%3DF_9jaMkCgyKxUQGtFQU7oN4kX-AA%40mail.gmail.com
    https://www.postgresql.org/message-id/20201005160355.byp74sh3ejsv7wrj%40development
    https://www.postgresql.org/message-id/CAFiTN-tzTTT2oqWdRGLv1dvvS5MC1W%2BLE%2B3bqWPJUZj4GnHOJg%40mail.gmail.com

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -2052,7 +2052,8 @@ ExecPrepareTupleRouting(ModifyTableState *mtstate,
 TupleTableSlot *
 CompareCompressionMethodAndDecompress(TupleTableSlot *slot,
 									  TupleTableSlot **outslot,
-									  TupleDesc targetTupDesc)
+									  TupleDesc targetTupDesc,
+									  bool *decompressed)
 {
 	int			i;
 	int			attnum;
@@ -2140,6 +2141,9 @@ CompareCompressionMethodAndDecompress(TupleTableSlot *slot,
 		ExecClearTuple(slot);
 
 		*outslot = newslot;
+
+		if (decompressed != NULL)
+			*decompressed = true;
 
 		return newslot;
 	}
@@ -2362,7 +2366,8 @@ ExecModifyTable(PlanState *pstate)
 		 */
 		slot = CompareCompressionMethodAndDecompress(slot,
 									&node->mt_decompress_tuple_slot,
-									resultRelInfo->ri_RelationDesc->rd_att);
+									resultRelInfo->ri_RelationDesc->rd_att,
+									NULL);
 
 		switch (operation)
 		{

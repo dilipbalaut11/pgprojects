8a41839b348565f079a2873fc2fcd04f7145904f
Dilip Kumar <dilipkumar@localhost.localdomain>
2021-08-31 15:15:00 +0530

    WAL logged CREATE DATABASE
    Currently, CREATE DATABASE forces a checkpoint, then copies all the files,
    then forces another checkpoint. The comments in the createdb() function
    explain the reasons for this. The attached patch fixes this problem by making
    create database completely WAL logged so that we can avoid the checkpoints.
    
    This can also be useful for supporting the TDE. For example, if we need different
    encryption for the source and the target database then we can not re-encrypt the
    page data if we copy the whole directory.  But with this patch, we are copying
    page by page so we have an opportunity to re-encrypt the page before copying that
    to the target database.

--- src/backend/access/rmgrdesc/dbasedesc.c
+++ src/backend/access/rmgrdesc/dbasedesc.c
@@ -24,12 +24,11 @@ dbase_desc(StringInfo buf, XLogReaderState *record)
 	char	   *rec = XLogRecGetData(record);
 	uint8		info = XLogRecGetInfo(record) & ~XLR_INFO_MASK;
 
-	if (info == XLOG_DBASE_CREATE)
+	if (info == XLOG_DBASEDIR_CREATE)
 	{
 		xl_dbase_create_rec *xlrec = (xl_dbase_create_rec *) rec;
 
-		appendStringInfo(buf, "copy dir %u/%u to %u/%u",
-						 xlrec->src_tablespace_id, xlrec->src_db_id,
+		appendStringInfo(buf, "create dir %u/%u",
 						 xlrec->tablespace_id, xlrec->db_id);
 	}
 	else if (info == XLOG_DBASE_DROP)
@@ -51,8 +50,8 @@ dbase_identify(uint8 info)
 
 	switch (info & ~XLR_INFO_MASK)
 	{
-		case XLOG_DBASE_CREATE:
-			id = "CREATE";
+		case XLOG_DBASEDIR_CREATE:
+			id = "CREATE DIR";
 			break;
 		case XLOG_DBASE_DROP:
 			id = "DROP";

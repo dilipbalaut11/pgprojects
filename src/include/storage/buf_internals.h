a7f69bfbfa64eee8267242c6538b76ad8679a21f
dilip kumar <dilipbalaut@localhost.localdomain>
2022-07-05 12:51:25 +0530

    Preliminary refactoring for supporting larger  relfilenumber
    Currently, relfilenumber is Oid type and it can wrap around so as part of
    the larger patch set we are trying to make it 64 bit to avoid wraparound
    and that will make a couple of other things simpler as explained in the
    next patches.
    
    So this is just a preliminary refactoring patch as part of this, in
    BufferTag, instead of keeping the RelFileLocator, we will keep the
    tablespace Oid, database Oid, and the relfilenumber directly.  So that
    once we change the relNumber in RelFileLocator to 64 bits the buffer tag
    alignment padding will not change.

--- src/include/storage/buf_internals.h
+++ src/include/storage/buf_internals.h
@@ -90,32 +90,59 @@
  */
 typedef struct buftag
 {
-	RelFileLocator rlocator;			/* physical relation identifier */
-	ForkNumber	forkNum;
-	BlockNumber blockNum;		/* blknum relative to begin of reln */
+	Oid				spcOid;			/* tablespace oid. */
+	Oid				dbOid;			/* database oid. */
+	RelFileNumber	relNumber;		/* relation file number. */
+	ForkNumber		forkNum;
+	BlockNumber 	blockNum;		/* blknum relative to begin of reln */
 } BufferTag;
+
+#define	BufTagGetFileNumber(a) ((a).relNumber)
+
+#define	BufTagSetFileNumber(a, relnumber) \
+( \
+	(a).relNumber = (relnumber) \
+)
 
 #define CLEAR_BUFFERTAG(a) \
 ( \
-	(a).rlocator.spcOid = InvalidOid, \
-	(a).rlocator.dbOid = InvalidOid, \
-	(a).rlocator.relNumber = InvalidRelFileNumber, \
+	(a).spcOid = InvalidOid, \
+	(a).dbOid = InvalidOid, \
+	BufTagSetFileNumber(a, InvalidRelFileNumber), \
 	(a).forkNum = InvalidForkNumber, \
 	(a).blockNum = InvalidBlockNumber \
 )
 
 #define INIT_BUFFERTAG(a,xx_rlocator,xx_forkNum,xx_blockNum) \
 ( \
-	(a).rlocator = (xx_rlocator), \
+	(a).spcOid = (xx_rlocator).spcOid, \
+	(a).dbOid = (xx_rlocator).dbOid, \
+	BufTagSetFileNumber(a, (xx_rlocator).relNumber), \
 	(a).forkNum = (xx_forkNum), \
 	(a).blockNum = (xx_blockNum) \
 )
 
 #define BUFFERTAGS_EQUAL(a,b) \
 ( \
-	RelFileLocatorEquals((a).rlocator, (b).rlocator) && \
+	(a).spcOid == (b).spcOid && \
+	(a).dbOid == (b).dbOid && \
+	(a).relNumber == (b).relNumber && \
 	(a).blockNum == (b).blockNum && \
 	(a).forkNum == (b).forkNum \
+)
+
+#define BufTagCopyRelFileLocator(a, locator) \
+do { \
+	(locator).spcOid = (a).spcOid; \
+	(locator).dbOid = (a).dbOid; \
+	(locator).relNumber = (a).relNumber; \
+} while(0)
+
+#define BufTagRelFileLocatorEquals(a, locator) \
+( \
+	(a).spcOid == (locator).spcOid && \
+	(a).dbOid == (locator).dbOid && \
+	(a).relNumber == (locator).relNumber \
 )
 
 /*

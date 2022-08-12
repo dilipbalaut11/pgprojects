2a8cca771f7e8c1b1c37539d8786b5aff37dc88b
Robert Haas <rhaas@postgresql.org>
2022-08-12 13:42:07 -0400

    XLogGetOldestSegno

--- src/include/access/xlog.h
+++ src/include/access/xlog.h
@@ -205,6 +205,7 @@ extern int	XLogFileOpen(XLogSegNo segno, TimeLineID tli);
 
 extern void CheckXLogRemoved(XLogSegNo segno, TimeLineID tli);
 extern XLogSegNo XLogGetLastRemovedSegno(void);
+extern XLogSegNo XLogGetOldestSegno(TimeLineID tli);
 extern void XLogSetAsyncXactLSN(XLogRecPtr record);
 extern void XLogSetReplicationSlotMinimumLSN(XLogRecPtr lsn);
 

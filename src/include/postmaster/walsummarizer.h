38041141d9b8f9901c513322d413b853c9d34ff0
Robert Haas <rhaas@postgresql.org>
2022-08-12 15:46:41 -0400

    add checkpointer interlock

--- src/include/postmaster/walsummarizer.h
+++ src/include/postmaster/walsummarizer.h
@@ -1,11 +1,15 @@
 #ifndef WALSUMMARIZER_H
 #define WALSUMMARIZER_H
 
+#include "access/xlogdefs.h"
+
 extern int wal_summarize_mb;
 extern int wal_summarize_keep_time;
 
 extern Size WalSummarizerShmemSize(void);
 extern void WalSummarizerShmemInit(void);
 extern void WalSummarizerMain(void) pg_attribute_noreturn();
+
+extern XLogRecPtr GetOldestUnsummarizedLSN(void);
 
 #endif

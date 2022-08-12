f16c2ec7d030f2ce463984788f0239e45f4dddf9
Robert Haas <rhaas@postgresql.org>
2022-08-12 13:40:45 -0400

    add some sleep governance logic and a (broken) attempt to read WAL

--- src/include/postmaster/walsummarizer.h
+++ src/include/postmaster/walsummarizer.h
@@ -1,4 +1,11 @@
+#ifndef WALSUMMARIZER_H
+#define WALSUMMARIZER_H
+
 extern int wal_summarize_mb;
 extern int wal_summarize_keep_time;
 
+extern Size WalSummarizerShmemSize(void);
+extern void WalSummarizerShmemInit(void);
 extern void WalSummarizerMain(void) pg_attribute_noreturn();
+
+#endif

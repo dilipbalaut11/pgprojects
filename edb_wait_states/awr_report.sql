d4ea6ec96d553d977b6c4727a0cbdc42c65d2d88
Dilip Kumar <dilip@localhost.localdomain>
2023-08-04 09:42:48 +0530

    new options in the report and new patch

--- edb_wait_states/awr_report.sql
+++ edb_wait_states/awr_report.sql
@@ -18,20 +18,6 @@ SELECT :start AS snap_start, :end AS snap_end;
 SELECT version() AS PostgreSQL_Version;
 
 \H
-\qecho '<h2>PostgreSQL Database Settings</h2>'
-\x on
-\t on
-\pset format html
-SELECT category, string_agg(name || '=' || setting, E'\n' ORDER BY name) As settings
-FROM pg_settings
-WHERE 1 = 1
-GROUP BY category
-ORDER BY category;
-
-\x off
-\t off
-
-\H
 \qecho '<h2>System Information</h2>'
 \pset format html
 select * from edb_wait_states_header();
@@ -50,11 +36,30 @@ SELECT date_trunc('second', current_timestamp - pg_postmaster_start_time()) as u
 \H
 \qecho '<h2>Top wait events</h2>'
 \pset format html
-select * from edb_wait_states_top_wait_events(:start, :end);
+select * from edb_wait_states_top_wait_events(:start, :end) ORDER BY waittime DESC LIMIT 10;
 \H
 \qecho '<h2>Top SQL statements</h2>'
 \pset format html
-select * from edb_wait_states_sql_statements(:start, :end);
+select * from edb_wait_states_sql_statements(:start, :end) ORDER BY dbtime DESC LIMIT 10;
+
+\H
+\qecho '<h2>Longest active session</h2>'
+\pset format html
+select *, (ref_end_ts - ref_start_ts) AS conection_duration from edb_wait_states_sessions() ORDER BY conection_duration DESC LIMIT 10;
+
+\H
+\qecho '<h2>PostgreSQL Database Settings</h2>'
+\x on
+\t on
+\pset format html
+SELECT category, string_agg(name || '=' || setting, E'\n' ORDER BY name) As settings
+FROM pg_settings
+WHERE 1 = 1
+GROUP BY category
+ORDER BY category;
+
+\x off
+\t off
 
 \qecho '</td></tr></table>'
 \qecho '</body></html>'

f4abfbc7ad354dc2992a103a6977129360af64f4
Dilip Kumar <dilipkumar@Dilip.local>
2025-05-15 16:59:31 +0530

    Provide Support for creating global indexes and other  DDL  on global indexes
    Syntax: CREATE INDEX name ON table (column_list) GLOBAL;
    
    As described in commit message of the previous patch TIDs alone are
    insufficient for uniquely identifying tuples in global indexes because
    they include tuples from multiple partitions. To uniquely identify heap
    tuples, we append a partitionID. For detailed information, refer to
    the comments in the patch 0001 commit message.
    
    In this design, the partitionID is included as the last key column of the
    index. The rationale behind storing it as the last key column is that in
    various scenarios, this column is treated as an extended index key column.
    For example, in B-tree indexes, each index tuple must be uniquely identified.
    If we encounter duplicate keys, we use heap TID as a tiebreaker. However,
    for global indexes, relying solely on heap TID isn't adequate; we also
    require the partition identifier. Including this as an additional key
    column simplifies the process, as index tuples are arranged in key order,
    and this column will automatically be part of that order.
    
    Whenever a global index is created, this patch utilizes the interfaces from
    the previous patch to assign a partitionID to each leaf relation of the
    partitioned table on which the global index is being created. It then inserts
    the (indexid, partitionID) -> reloid mapping into the pg_index_partition table
    for each leaf relation.
    
    Additionally, we need to create this mapping whenever a partition is attached.
    Specifically, we must create mappings for all leaf partitions of the table being
    attached, corresponding to the global indexes on the parent and all its ancestors
    under which the new partition is being attached.
    
    Similarly, when a partition is detached, we must invalidate the mappings for all
    leaf partitions under the detached partition related to the global indexes on all
    ancestors from which the partition is being detached.
    
    Open Items:
    - Rebuilding the global indexes when truncating the relation and reindexing the relation.
      currently global indexes are getting reindexed for each partition whereas it should be done only once
    
    - Currently parition id column is treated as special column, Robert suggested to do it as an expression
    
    - Vacuum is vacuuming global indexes multiple times for each leaf partition, this need to be optimized
    
    - RelationGetIndexList is not follwoing the locking order i.e. parent to child as the child might already be locked
    this is causing a deadlock, need to fix this

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -35,6 +35,7 @@
 #include "access/transam.h"
 #include "access/xact.h"
 #include "catalog/namespace.h"
+#include "catalog/partition.h"
 #include "catalog/pg_database.h"
 #include "catalog/pg_inherits.h"
 #include "commands/cluster.h"
@@ -58,6 +59,7 @@
 #include "utils/guc_hooks.h"
 #include "utils/memutils.h"
 #include "utils/snapmgr.h"
+#include "utils/lsyscache.h"
 #include "utils/syscache.h"
 
 /*
@@ -2346,6 +2348,10 @@ vac_open_indexes(Relation relation, LOCKMODE lockmode,
 
 	Assert(lockmode != NoLock);
 
+	/*
+	 * Get list of all the indexes including the global indexes of all its
+	 * ancestors.
+	 */
 	indexoidlist = RelationGetIndexList(relation);
 
 	/* allocate enough memory for all indexes */
@@ -2364,7 +2370,8 @@ vac_open_indexes(Relation relation, LOCKMODE lockmode,
 		Relation	indrel;
 
 		indrel = index_open(indexoid, lockmode);
-		if (indrel->rd_index->indisready)
+		if (indrel->rd_index->indisready &&
+			indrel->rd_index->indrelid == RelationGetRelid(relation))
 			(*Irel)[i++] = indrel;
 		else
 			index_close(indrel, lockmode);

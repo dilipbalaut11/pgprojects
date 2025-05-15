6d171d2269d4a368e66daab758babcf46ea37a0c
Dilip Kumar <dilipkumar@Dilip.local>
2025-05-15 17:39:58 +0530

    Provide support for global Index Scan Path
    In previous patches we have added support for creating the global index.  Now
    in this patch we provided a support in planner to choose a global index scan
    and index only scan paths at for the append rel.
    
    Currently we do not have support for selecting a bitmap scan using the global
    index.  We may do that in future and if we need to do that we need to change
    a executor such that we can build a sperate tidmap for each leaf relation while
    scanning the global index and then do the bitmap heap scan partition at a time
    based on the bitmap.
    
    We also do not support the parallel index scan using the global index.  There
    is nothing blocking as such but this is still a TODO.
    
    Open Items
    - ROWID_VAR is not cleanly handled in optimized, for now just made it workable but need to fix it properly
    - In table_slot_callbacks(), now partiioned table can generate tuple by global index scan so we need proper
    slot instead of just assigning a virtual slot.  This handling should be done maybe through AM callback?

--- src/backend/utils/adt/selfuncs.c
+++ src/backend/utils/adt/selfuncs.c
@@ -6499,6 +6499,8 @@ get_actual_variable_range(PlannerInfo *root, VariableStatData *vardata,
 		/* Ignore non-ordering indexes */
 		if (index->sortopfamily == NULL)
 			continue;
+		if (index->idxkind != INDEX_LOCAL)
+			continue;
 
 		/*
 		 * Ignore partial indexes --- we only want stats that cover the entire
@@ -6718,6 +6720,8 @@ get_actual_variable_endpoint(Relation heapRel,
 	 */
 	InitNonVacuumableSnapshot(SnapshotNonVacuumable,
 							  GlobalVisTestFor(heapRel));
+
+	Assert(!RelationIsGlobalIndex(indexRel));
 
 	index_scan = index_beginscan(heapRel, indexRel,
 								 &SnapshotNonVacuumable, NULL,

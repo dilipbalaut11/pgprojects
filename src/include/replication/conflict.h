8ebaffe83b9a4a1bf1229a0bf46a49962fea2f90
Dilip Kumar <dilipkumarb@google.com>
2025-12-20 15:20:09 +0530

    Implement the conflict insertion infrastructure for the conflict log table
    This patch introduces the core logic to populate the conflict log table whenever
    a logical replication conflict is detected. It captures the remote transaction
    details along with the corresponding local state at the time of the conflict.
    
    Handling Multi-row Conflicts: A single remote tuple may conflict with multiple
    local tuples (e.g., in the case of multiple_unique_conflicts). To handle this,
    the infrastructure creates a single row in the conflict log table for each
    remote tuple. The details of all conflicting local rows are aggregated into a
    single JSON array in the local_conflicts column.
    
    The JSON array uses the following structured format:
    [ { "xid": "1001", "commit_ts": "2025-12-25 10:00:00+05:30", "origin": "node_1",
    "key": {"id": 1}, "tuple": {"id": 1, "val": "old_data"} }, ... ]
    
    Example of querying the structured conflict data:
    
    SELECT remote_xid, relname, remote_origin, local_conflicts[1] ->> 'xid' AS local_xid,
           local_conflicts[1] ->> 'tuple' AS local_tuple
    FROM myschema.conflict_log_history2;
    
     remote_xid | relname  | remote_origin | local_xid |     local_tuple
    ------------+----------+---------------+-----------+---------------------
            760 | test     | pg_16406      | 771       | {"a":1,"b":10}
            765 | conf_tab | pg_16406      | 775       | {"a":2,"b":2,"c":2}

--- src/include/replication/conflict.h
+++ src/include/replication/conflict.h
@@ -132,7 +132,6 @@ static const ConflictLogColumnDef ConflictLogSchema[] =
 	{ .attname = "local_conflicts",  .atttypid = JSONARRAYOID }
 };
 
-/* Define the count using the array size */
 #define MAX_CONFLICT_ATTR_NUM (sizeof(ConflictLogSchema) / sizeof(ConflictLogSchema[0]))
 
 extern bool GetTupleTransactionInfo(TupleTableSlot *localslot,
@@ -145,4 +144,7 @@ extern void ReportApplyConflict(EState *estate, ResultRelInfo *relinfo,
 								TupleTableSlot *remoteslot,
 								List *conflicttuples);
 extern void InitConflictIndexes(ResultRelInfo *relInfo);
+extern Relation GetConflictLogTableInfo(ConflictLogDest *log_dest);
+extern void InsertConflictLogTuple(Relation conflictlogrel);
+extern bool ValidateConflictLogTable(Relation rel);
 #endif
